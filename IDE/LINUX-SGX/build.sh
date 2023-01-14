838c8db088cfa9f2d49ee51a6072284f06cc2105
LihengChen <791960492@qq.com>
2023-01-15 04:44:02 +0800

    sgxfuzz

--- IDE/LINUX-SGX/build.sh
+++ IDE/LINUX-SGX/build.sh
@@ -1,9 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
+# same OPTION for nonSGX version
+# ./configure --enable-rabbit --enable-des3 --disable-sha224 --disable-sha384 --disable-sha512 --disable-sha3 --disable-poly1305 --disable-chacha CC=clang CXX=clang++ CCAS=clang
 
-CFLAGS_NEW="-DDEBUG_WOLFSSL"
+CFLAGS_NEW="-DGCM_TABLE_4BIT"
 export CFLAGS="${CFLAGS} ${CFLAGS_NEW}"
 echo ${CFLAGS}
 
-make -f sgx_t_static.mk HAVE_WOLFSSL_BENCHMARK=1 HAVE_WOLFSSL_TEST=1 HAVE_WOLFSSL_SP=1
+if [[ $1 == 'debug' ]]; then
+    DEBUG_FLAG="SGX_DEBUG=1 SGX_PRERELEASE=0"
+else
+    DEBUG_FLAG="SGX_DEBUG=0 SGX_PRERELEASE=1"
+fi
+
+make -f sgx_t_static.mk HAVE_WOLFSSL_BENCHMARK=1 HAVE_WOLFSSL_TEST=1 HAVE_WOLFSSL_SP=1 ${DEBUG_FLAG} -j$(nproc)
 
